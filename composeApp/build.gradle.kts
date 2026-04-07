2c9013e35f7f8fb70293c00e0bfbad1aedb5b3e0
brunogarcia25941 <aluno25941@ipt.pt>
2026-04-07 15:41:37 +0100

    Atualizar versão da aplicação para 1.1 (build 2)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -93,8 +93,8 @@ android {
         applicationId = "com.brunogarcia.shoppinglist"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     packaging {
         resources {
